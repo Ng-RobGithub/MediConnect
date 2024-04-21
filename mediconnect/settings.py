17e09fb1741b32ad14b5cdfc7efe3f67d5327753
Ng-RobGithub <ngrob4real@gmail.com>
2024-04-21 05:42:51 -0700

    commit stettings update

--- mediconnect/settings.py
+++ mediconnect/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Define the directory where Django should look for static files
+STATICFILES_DIRS = [
+BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
